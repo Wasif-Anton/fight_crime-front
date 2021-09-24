4e1cbd8c1805b221cd7e79bc6859f635d7b58193
Elias <elias.29@hotmail.com>
2021-09-25 00:40:17 +0300

    add more camponents file

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import "./Navbar.css"
+
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <img src="" alt="safsdfsd"/>
+                <ul>
+                    <li>About Us</li>
+                    <li>Team</li>
+                    <li>Project</li>
+                    <li>Statistics</li>
+                    <li>Join</li>
+                </ul>
+            </div>
+        )
+    }
+}
