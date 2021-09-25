5496ad100ee296e11001570949d84c8d0020b2d7
Elias <elias.29@hotmail.com>
2021-09-25 16:07:51 +0300

    new image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import "./Navbar.css"
+import Logo from "../../img/Talens.png"
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
+            <nav>
                 <ul className="nav-bar">
-                <img src="" alt="Logo"/>
+                <img src={Logo} alt="Logo"/>
                     <li>About Us</li>
                     <li>Team</li>
                     <li>Project</li>
                     <li>Statistics</li>
                     <li>Join</li>
                 </ul>
-            </div>
+            </nav>
         )
     }
 }
